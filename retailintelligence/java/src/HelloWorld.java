81f3bf12ec5943f59169e00e40cf1852bc8b2a6d
dataanlytics2016 <basu.bharadwaj@datanalyticservices.com>
2016-06-08 21:03:37 +0530

    Code change
    HelloWorld.java file updated

--- retailintelligence/java/src/HelloWorld.java
+++ retailintelligence/java/src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
 	public static void main(String args[]){
 		System.out.println("Welcome to Data Analytics.");
+		System.out.println("Welcome to Data AnalytiCS. Start coding");
 	}
 }
